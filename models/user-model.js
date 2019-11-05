4640591cd9685dd4037036aa475b9f08ba52f8a7
Jeff Lapp <lappjeff20@gmail.com>
2019-11-05 17:34:44 -0500

    Added skeletons for user model functions

--- models/user-model.js
+++ models/user-model.js
@@ -1,3 +1,31 @@
-const findUserById = id => {};
+const db = require("../data/dbConfig.js");
+const { calculateMacros } = require("./model-helpers");
 
-module.exports = {};
+module.exports = {
+	findUserById,
+	updateUser,
+	deleteUser,
+	createUser,
+	getCurrentUser
+};
+
+function updateUser(user_id, updates) {}
+
+function deleteUser(user_id) {}
+
+async function createUser(user) {
+	const userMacros = calculateMacros(user);
+
+	const finalMacros = { ...user, user_macros: userMacros };
+
+	const user = await db("users").insert(finalUser);
+	console.log(user);
+	return user;
+}
+
+function getCurrentUser() {}
+function findUserById(user_id) {
+	return db("users")
+		.where({ user_id })
+		.first();
+}
