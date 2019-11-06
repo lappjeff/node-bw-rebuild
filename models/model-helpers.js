618c0c79271b128a01f6a04322e30d642488ade9
Jeff Lapp <lappjeff20@gmail.com>
2019-11-05 19:14:27 -0500

    Created macro calculation helper

--- models/model-helpers.js
+++ models/model-helpers.js
@@ -1,5 +1,65 @@
+const validators = require("../validation/user-validators");
+
 module.exports = {
 	calculateMacros
 };
 
-function calculateMacros(user) {}
+function calculateMacros(user) {
+	const bmrFormulas = {
+		m: (weight, height, age) => 66 + 6.23 * weight + 12.7 * height - 6.8 * age,
+		f: (weight, height, age) => 655 + 4.35 * weight + 4.7 * height - 4.5 * age
+	};
+
+	const activityMultipliers = {
+		"0 days": 1.2,
+		"1-2 days": 1.375,
+		"3-4 days": 1.55,
+		"5-6 days": 1.725,
+		"7 days": 1.9
+	};
+
+	const goalMultipliers = {
+		"aggressive weight loss": 0.8,
+		"moderate weight loss": 0.85,
+		"weight loss": 0.9,
+		"maintain weight": 1,
+		"moderate weight gain": 1.1,
+		"aggressive weight gain": 1.15
+	};
+
+	const validateUser = validators.validateUser(user);
+	if (validateUser.isValidUser) {
+		const heightArray = user.height.split("'");
+		const heightInches = heightArray[0] * 12 + parseInt(heightArray[1]);
+
+		const bmr = bmrFormulas[user.gender.toLowerCase()](
+			user.current_weight,
+			heightInches,
+			user.age
+		);
+
+		const tdee =
+			bmr *
+			activityMultipliers[user.activity_lvl.toLowerCase()] *
+			goalMultipliers(user.goal.toLowerCase());
+
+		const [dailyProtein, dailyCarbs, dailyFat] = [
+			tdee * 0.075,
+			tdee * 0.1,
+			tdee * 0.033
+		];
+
+		const userMacros = {
+			dailyCalories: tdee,
+			dailyProtein,
+			dailyCarbs,
+			dailyFat
+		};
+
+		return userMacros;
+	} else {
+		return { errors: validateUser.errors };
+	}
+}
+
+console.log(calculateMacros({}));
