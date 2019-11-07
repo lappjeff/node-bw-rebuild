520c3fb5db288646b0a467bf358175cb9a2d23d7
Jeff Lapp <lappjeff20@gmail.com>
2019-11-07 12:19:28 -0500

    Finalized throw format for validateUser, modified gitignore

--- helpers/user-validator.js
+++ helpers/user-validator.js
@@ -1,32 +1,43 @@
 module.exports = { validateUser };
 
 function validateUser(user) {
+	const errors = [];
+
 	if (!user.username) {
-		throw "Please provide a username";
+		errors.push("Please provide a username");
 	}
 	if (!user.password) {
-		throw "Please provide a password";
+		errors.push("Please provide a password");
 	}
 	if (!user.email) {
-		throw "Please provide a email";
+		errors.push("Please provide a email");
 	}
 	if (!user.gender || user.gender.length > 1) {
-		throw "Please provide a valid gender(M/F)";
+		errors.push("Please provide a valid gender(M/F)");
 	}
 	if (!user.activity_lvl) {
-		throw "Please provide a weekly activity level: 1-2, 3-4, 5-6, or 7 days.";
+		errors.push(
+			"Please provide a weekly activity level: 1-2, 3-4, 5-6, or 7 days."
+		);
 	}
 	if (!user.goal) {
-		throw "Please provide a valid goal: aggressive weight loss, moderate weight loss, weight loss, maintain weight, moderate weight gain, aggressive weight gain";
+		errors.push(
+			"Please provide a valid goal: aggressive weight loss, moderate weight loss, weight loss, maintain weight, moderate weight gain, aggressive weight gain"
+		);
 	}
 	if (!user.height) {
-		throw "Please provide a valid height in format feet/inches(5'7)";
+		errors.push("Please provide a valid height in format feet/inches(5'7)");
 	}
 	if (!user.age) {
-		throw "Please provide a valid age";
+		errors.push("Please provide a valid age");
 	}
 	if (!user.current_weight) {
-		throw "Please provide a valid weight in lbs";
+		errors.push("Please provide a valid weight in lbs");
 	}
-	return true;
+
+	if (errors.length > 0) {
+		throw errors;
+	} else {
+		return true;
+	}
 }
