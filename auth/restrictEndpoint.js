8614ff62ee4d34d526d3a97bb4d6b68ab8a4f4f9
Jeff Lapp <lappjeff20@gmail.com>
2019-11-12 16:12:47 -0500

    Removed protectRoute helper and imports, added error to restrictEndpoint

--- auth/restrictEndpoint.js
+++ auth/restrictEndpoint.js
@@ -3,13 +3,10 @@ const jwt = require("jsonwebtoken");
 const restrictEndpoint = (req, res, next) => {
 	const token = req.headers.authorization;
 
-	console.log("-----------------------------", process.env.TOKEN_SECRET);
 	if (token) {
 		jwt.verify(token, process.env.TOKEN_SECRET, (err, decodedToken) => {
 			if (err) {
-				console.log(err);
-
-				res.status(401).json({ message: "Invalid token" });
+				res.status(401).json({ message: "Invalid token", error: err });
 			} else {
 				req.username = decodedToken.username;
 				next();
--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const protected = require("../../auth/protectRoute");
 const Users = require("../../models/user-model");
 
 const validateUser = require("../../helpers/user-validator");
 
+const restricted = require("../../auth/restrictEndpoint");
+
+router.get("/testtoken", restricted, (req, res) => {
+	res.status(200).json({ message: `Welcome ${req.username}` });
+});
 module.exports = router;
