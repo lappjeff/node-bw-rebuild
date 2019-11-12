195f573587b2c8548c70f35b8dc0b2576cf5f583
Jeff Lapp <lappjeff20@gmail.com>
2019-11-12 15:19:10 -0500

    Removed createUser route from userRoutes

--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -3,15 +3,5 @@ const protected = require("../../auth/protectRoute");
 const Users = require("../../models/user-model");
 
 const validateUser = require("../../helpers/user-validator");
-router.post("/", async (req, res) => {
-	try {
-		validateUser(req.body);
-		const user = await Users.createUser(req.body);
-
-		res.status(201).json({ message: "User created", id: user });
-	} catch (errors) {
-		res.status(500).json({ message: "User could not be created", errors });
-	}
-});
 
 module.exports = router;
