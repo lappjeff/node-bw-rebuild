0efd66d634efa476c6af908885d66fc784776a88
Jeff Lapp <lappjeff20@gmail.com>
2019-11-12 16:43:05 -0500

    Removed final ref to protectRoute

--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const protected = require("../../auth/protectRoute");
 const Users = require("../../models/user-model");
 
 const validateUser = require("../../helpers/user-validator");
