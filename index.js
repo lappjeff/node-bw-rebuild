f6371256d0d50f08dbe86d8e27199ae91146aac6
Jeff Lapp <lappjeff20@gmail.com>
2019-11-12 14:23:09 -0500

    Added jsonwebtoken, started work on auth

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const knex = require("knex");
 const knexConfig = require("./knexfile");
 const userRouter = require("./routes/user/userRoutes.js");
+const authRouter = require("./routes/auth/authRoutes.js");
 
 const server = express();
 
 server.use(express.json());
 
 server.use("/api/users", userRouter);
+server.use("/api/auth", authRouter);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server running on port ${port}`));
