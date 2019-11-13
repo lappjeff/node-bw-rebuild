a7d8c87d4a5da776524bc74a25beda18460c64e3
Jeff Lapp <lappjeff20@gmail.com>
2019-11-12 19:49:15 -0500

    Finished delete and update routes

--- auth/verifyCredentials.js
+++ auth/verifyCredentials.js
@@ -3,7 +3,8 @@ const bcrypt = require("bcrypt");
 module.exports = verifyCredentials;
 
 async function verifyCredentials(req, res, next) {
-	const { username, password } = req.body || req.headers;
+	const { username, password } = req.headers;
+
 	if (username && password) {
 		const user = await findByUsername(username);
 		if (user && bcrypt.compareSync(password, user.password)) {
--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -2,14 +2,26 @@ const router = require("express").Router();
 const Users = require("../../models/user-model");
 const verify = require("../../auth/verifyCredentials");
 
-router.delete("/delete", verify, async (req, res) => {
+router.use(verify);
+
+router.delete("/delete", async (req, res) => {
 	try {
 		Users.deleteUser(req.user.user_id);
-		res.status(200);
+		res.status(204).end();
 	} catch (err) {
 		res.status(500).json({ message: "User could not be deleted", error: err });
 	}
 });
 
-router.put("/update", verify, async (req, res) => {});
+router.put("/update", async (req, res) => {
+	try {
+		const updates = req.body;
+		Users.updateUser(req.user.user_id, updates);
+		res
+			.status(200)
+			.json({ message: `User ${req.user.user_id} successfully updated` });
+	} catch (err) {
+		res.status(500).json({ message: "User could not be updated", error: err });
+	}
+});
 module.exports = router;
